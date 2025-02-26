fd21ced5820de702d904ba520f0b2ff4ff3b402e
Aarit-Singh <aaritsingh2588@gmail.com>
2025-02-26 16:41:26 -0500

    Update NYKF-Final.py

--- NYKF-Final.py
+++ NYKF-Final.py
@@ -1,15 +1,14 @@
 """
+     
      Basically, before i wrote this the person who ran the kathak festival would have to manually tally the seats in each category, 
      to decide how many seats to take out of stock to house VIPS like indian consulate members (who came to watch the show sometimes)
      and donors. 
      This code streamlined that process and eliminated th tallying phase by sending an email with the tallies, preventing someone from wasing 15mins downloading and combing through an excell file.
-     Here are my prototypes (This is the final one), if you want to see all of the prototypes email 81516100@briarcliffny.org 
-         first, i simply created a program which would do the tallying using a CSV file - realized that is an awful idea as they would ahve to run a piece of code
-         and manually download the file every time they needed to check order status.
-         I solved with with a restful API call using the python requests library. i then used the orders API and attempted to make a notification appear.
-         this idea was scrapped due to the fact that the laptop had to be open for the code to run and produce a notification 
-         this was the 4rd and final apporach (first tried using orders API which had a 50-item limit, same aproach as above, landed on inventory api),
-         where a scheduler would send an email to the recipiant email address
+     this is the fourth itteration:
+          1st - simple CSV counter
+          2nd - orders api notifications
+          3rd - inventory api notifications
+          4th - (this one) inventory api email
 """
 import requests
 import json
@@ -23,6 +22,7 @@ def send_task():
     api_key = "-" #not leaving my API key in a public github repo
     response = requests.get("https://api.squarespace.com/1.0/commerce/inventory?cursor=NWUzOGVhMTM0OTY2ODMyYWE3MWM0ZTU4OlNRMTI2ODQzOQ", headers={"Authorization" : "Bearer bfee1bcd-6a00-4299-af01-e6eb448a0af6"})
     #processing data and counting oders
+     # base_data is the base data for this program which gets modified. this contains the inventory of the NYKF.
     with open("base_data.json", "wb") as f:
         print("a")
         f.write(response.content)
